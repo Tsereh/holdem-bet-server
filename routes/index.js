c915b1d73226c01659633fae26d390c16d2de498
Tsereh <tserehbros@gmail.com>
2019-03-10 21:50:05 +0200

    heroku setup

--- routes/index.js
+++ routes/index.js
@@ -90,7 +90,11 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+server.listen(port, () => {
   console.log("Node app is running on port 3000");
 });
 
